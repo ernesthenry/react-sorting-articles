b4521e7f85617a329c20b58fc3860315a16847b5
ernesthenry <henry38ernest@gmail.com>
2024-07-31 17:32:11 +0300

    first commit

---
+++ src/components/Articles.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function Articles({ articles }) {
+  return (
+    <div className='card w-50 mx-auto'>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Upvotes</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {articles.length > 0 ? (
+            articles.map((art, i) => (
+              <tr data-testid='article' key={i}>
+                <td data-testid='article-title'>{art.title}</td>
+                <td data-testid='article-upvotes'>{art.upvotes}</td>
+                <td data-testid='article-date'>{art.date}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan='3'>No articles available</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Articles;
